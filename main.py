098fb0d0de7b1472d52dcb024fab65fa749a1060
nearworld <artemismars2@outlook.com>
2024-01-01 10:19:15 +0900

    feat: insert method for singly linked list

--- main.py
+++ main.py
@@ -7,3 +7,30 @@ class Node:
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+
+    def insert(self, index, data):
+        if index >= 0:
+            new_node = Node(data, None)
+            if self.head is None:
+                self.head = new_node
+            else:
+                i = 0
+                current = self.head
+                prev = current
+                while i < index and current.next is not None:
+                    prev = current
+                    current = current.next
+                    i += 1
+                prev.next = new_node
+                new_node.next = current.next
+
+
+linkedList = SinglyLinkedList()
+linkedList.insert(0, 10)
+linkedList.insert(1, 20)
+linkedList.insert(2, 30)
+linkedList.insert(3, 40)
+
+linkedList.insert(1, 100)
+
+print(linkedList.head.next.data)
