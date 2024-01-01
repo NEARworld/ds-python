3a500432991c5fcba73649e18952337bf9d35e69
nearworld <artemismars2@outlook.com>
2024-01-01 12:19:59 +0900

    fix: circular linked list logic to singly linked list one in insert method

--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -17,12 +17,14 @@ class SinglyLinkedList:
                 i = 0
                 current = self.head
                 prev = current
-                while i < index and current.next is not None:
+
+                while i < index and current is not None:
                     prev = current
                     current = current.next
                     i += 1
                 prev.next = new_node
-                new_node.next = current.next
+                if current:
+                    new_node.next = current.next
 
     def delete(self, index):
         if self.head is None:
@@ -37,6 +39,15 @@ class SinglyLinkedList:
                 i += 1
             prev.next = current.next
 
+    def traverse(self):
+        if self.head is None:
+            print("The list is empty")
+        else:
+            current = self.head
+            while current:
+                print(current.data)
+                current = current.next
+
 
 linkedList = SinglyLinkedList()
 linkedList.insert(0, 10)
@@ -45,7 +56,8 @@ linkedList.insert(2, 30)
 linkedList.insert(3, 40)
 
 linkedList.insert(1, 100)
-
-print(linkedList.head.next.data)  # 100
-linkedList.delete(1)
-print(linkedList.head.next.data)  # 30
+#
+# print(linkedList.head.next.data)  # 100
+# linkedList.delete(1)
+# print(linkedList.head.next.next.next.next.next.next.data)  # 30
+linkedList.traverse()
